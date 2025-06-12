ec37b180f2c3ed56fb0db17b892da6df53af2790
ElviraKukhtaruk <elvirakukhtaruk@gmail.com>
2025-06-12 23:54:55 +0200

    Figlet text

--- client/client.js
+++ client/client.js
@@ -1,11 +1,22 @@
 let handshake = require('./handshake/ECDHHandshake');
 let Response = require('./response/Response');
-
+const figlet = require('figlet');
 // Init Responses
 require('./response/initResponses');
 
+
 async function connection() {
 	try {
+
+		figlet.text('TERMINAL CHAT', {
+  			font: 'Big', 
+  			horizontalLayout: 'default',
+  			verticalLayout: 'default',
+
+		}, function(err, data) {
+  			console.log(data);
+		});
+
 		let client = this.client;
 
 		await handshake.newConnection(client, this.serverPublicKey);
