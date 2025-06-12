66fa74270b184ecd51ca7c91e23411a562dad0ac
ElviraKukhtaruk <elvirakukhtaruk@gmail.com>
2025-06-13 00:34:25 +0200

    configuration template

---
+++ server/configuration/mainConfig-template.js
@@ -0,0 +1,15 @@
+module.exports = {
+  DB: {
+    user: 'myuser',
+    host: 'localhost',
+    database: 'mydb',
+    password: 'mypassword',
+    port: 5432
+  },
+  REDIS: {
+    host: 'localhost',
+    port: 6379
+  }
+
+};
+
